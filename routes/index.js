200370e8b797557569b4712421ca5a494855135b
Laura Font <laurafontsalvador@gmail.com>
2020-04-06 09:08:00 +0200

    updated readme files

--- routes/index.js
+++ routes/index.js
@@ -32,8 +32,8 @@ router.get("/category/:id", function(req, res, next) {
 });
 
 // GET one resource by type
-router.get("/type/:id", function(req, res, next) {
-  db(`SELECT * FROM resources WHERE type_id=${req.params.id};`)
+router.get("/age/:id", function(req, res, next) {
+  db(`SELECT * FROM resources WHERE age_id=${req.params.id};`)
     .then(results => {
       res.send(results.data);
     })
@@ -43,7 +43,7 @@ router.get("/type/:id", function(req, res, next) {
 // INSERT a new resource into the DB
 router.post("/", function(req, res, next) {
   db(
-    `INSERT INTO resources (name, description, thumbnail, indoor, age, category_id, type_id) VALUES ('${req.body.name}', '${req.body.description}', '${req.body.thumbnail}', '${req.body.indoor}', '${req.body.age}', '${req.body.category_id}', '${req.body.type_id}');`
+    `INSERT INTO resources (name, description, thumbnail, indoor, file, category_id, age_id) VALUES ('${req.body.name}', '${req.body.description}', '${req.body.thumbnail}', '${req.body.indoor}', '${req.body.file}', '${req.body.category_id}', '${req.body.age_id}');`
   )
   .then(results => {
     res.send(results.data);
